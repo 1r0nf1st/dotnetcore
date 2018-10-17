8edac4363ebadcd74f7b3b161fff7ea55b260d1f
1r0nF1st <>
2018-10-17 22:45:38 +0100

    sql Change

--- _Database/customer.sql
+++ _Database/customer.sql
@@ -1,6 +1,6 @@
 --CREATE DATABASE Details;
 
-CREATE TABLE [Details].dbo.[Customer]
+/* CREATE TABLE [Details].dbo.[Customer]
 (
     PersonID int,
     Surname varchar(255),
@@ -8,10 +8,11 @@ CREATE TABLE [Details].dbo.[Customer]
     Email varchar(100),
     UserName varchar(50) 
 )
-
-INSERT INTO [Details].dbo.[Customer]
+ */
+/* INSERT INTO [Details].dbo.[Customer]
 (PersonID, Surname, FirstName, Email, UserName)
 VALUES(1, 'Williams', 'Jon', 'jon@w.com', 'jonw')
-
+ */
 select *
 from [Details].dbo.[Customer]
+----
